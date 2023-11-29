0583ef3de10cda441dc9d87c817b079038080bf5
Anne-Sophie <annesophie.faustino@gmail.com>
2023-11-29 08:30:38 +0400

    model

---
+++ server/model/user.model.js
@@ -0,0 +1,36 @@
+import { DataTypes } from "sequelize";
+import connectDB from "../database/database.js";
+
+const User = connectDB.define("users", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  nom: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  prenom: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  etablissementId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+export default User;
