d3ad2bcf41ec165679eade921a61cce2126c715d
User-sam-22 <samclo97400@gmail.com>
2023-12-06 23:30:17 +0400

    1er commit

---
+++ rest_flash_web/src/App.js
@@ -0,0 +1,171 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section id="fixed-bars">
+      <div id="menu">
+        {/* DEBUT SIDEBAR */}
+        <div id="side-bar">
+          <div id="logo-dashboard">
+            <img src="logo-departement.png" />
+          </div>
+          <div id="menu-side">
+            <ul id="navlink">
+              <li>
+                <span class="material-symbols-outlined">grid_view</span>
+                <p>
+                  <a href="/dashboard">Dashboard</a>
+                </p>
+              </li>
+              <li>
+                <span class="material-symbols-outlined">add</span>
+                <p>
+                  <a href="ajouter">Ajouter</a>
+                </p>
+              </li>
+              <li>
+                <span class="material-symbols-outlined">cycle</span>
+                <p>
+                  <a href="modifier">Modifier</a>
+                </p>
+              </li>
+              <li>
+                <span class="material-symbols-outlined">remove</span>
+                <p>
+                  <a href="supprimer">Supprimer</a>
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div id="logout">
+            <div id="btn-logout">
+              <span class="material-symbols-outlined">logout</span>
+              <h4>Déconnexion </h4>
+            </div>
+          </div>
+        </div>
+        {/* FIN SIDEBAR */}
+      </div>
+      <section id="right-content">
+        <div id="log-info">
+          <span class="material-symbols-outlined">person</span>
+          <div id="name-email">
+            <p id="username">Username</p>
+            <p id="email">adresse@gmail.com</p>
+          </div>
+        </div>
+
+        <section id="content">
+          <table>
+            <thead id="row-info">
+              <tr>
+                <td>Employé</td>
+                <td>Date</td>
+                <td>Quantité</td>
+              </tr>
+            </thead>
+            <tbody id="liste">
+              <tr>
+                <td>Giraud</td>
+                <td>Pierre</td>
+                <td>28</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+              <tr>
+                <td>Joly</td>
+                <td>Pauline</td>
+                <td>27</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </section>
+    </section>
+  );
+}
+
+export default App;
---
+++ rest_flash_web/src/Login.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import "./Login.css";
+import urlAPI from "./axios.config";
+
+function Login() {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
+
+  const handleSubmit = async (e)=>{
+    
+    
+    try {
+      e.preventDefault()
+      const response = await urlAPI.post("/connexion", {email, password});
+      console.log(response)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    //handleSubmit();
+  },[])
+  return (
+    <div className="Login">
+      <div id="div-img">
+        <img src="logo-departement.png" />
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <h1>Connexion</h1>
+        </div>
+        <div id="login-input">
+          <input type="text"
+           onChange={(e) => setEmail(e.target.value)} 
+           value={email} 
+           required
+           placeholder="Email" />
+          <input type="password"
+           onChange={(e) => setPassword(e.target.value)} 
+           value={password} 
+           required
+          placeholder="Mot de Passe" />
+        </div>
+        <div id="login-submit">
+          <input type="submit" value="Envoyer" />
+        </div>
+
+        <div id="extra-form">
+          <p id="forgotten-password">
+            <a href="">Mot de passe oublié?</a>
+          </p>
+          <div id="remember-me">
+            <input type="checkbox" name="remeber me" />
+            <p>Se souvenir de moi</p>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+export default Login;
